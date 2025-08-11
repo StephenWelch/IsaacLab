36f8805dbd9b4af6e0bc8498246182026c27b58e
Stephen Welch <stephenwelchva@vt.edu>
2025-08-10 22:52:33 -0500

    WIP

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source $$(conda info --base)/etc/profile.d/conda.sh && conda activate env_isaaclab && conda install -c conda-forge -y pinocchio && python scripts/reinforcement_learning/rsl_rl/train.py --headless --wandb-project angmom-loco --logger wandb --task Sandbox-Velocity-Flat-G1-v0 --run_name test
+source $(conda info --base)/etc/profile.d/conda.sh && conda activate env_isaaclab && conda install -c conda-forge -y pinocchio && python scripts/reinforcement_learning/rsl_rl/train.py --headless --wandb-project angmom-loco --logger wandb --task Sandbox-Velocity-Flat-G1-v0 --run_name test
