61274969a86b14f3df60b8e58e03239339267c6c
Stephen Welch <stephenwelchva@vt.edu>
2025-08-17 05:00:16 -0500

    Updates from upstream + 5.0 docker

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -2,5 +2,12 @@
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate env_isaaclab
+
+# Pinocchio/CasADi installation
 conda install -c conda-forge -y pinocchio
-python scripts/reinforcement_learning/rsl_rl/train.py --headless --log_project_name angmom-loco --logger wandb --task Sandbox-Velocity-Flat-G1-v0 --run_name test
+mkdir -p "$CONDA_PREFIX/etc/conda/activate.d"
+printf 'export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:${LD_LIBRARY_PATH}"\n' > "$CONDA_PREFIX/etc/conda/activate.d/000_set_ld_library_path.sh"
+conda deactivate && conda activate env_isaaclab
+
+# python scripts/reinforcement_learning/rsl_rl/train.py --headless --log_project_name angmom-loco --logger wandb --task Sandbox-Velocity-Flat-G1-v0 --run_name test
+python scripts/reinforcement_learning/rsl_rl/train.py --headless --log_project_name angmom-loco --logger wandb --task Isaac-Velocity-Flat-G1-v0 --run_name test
