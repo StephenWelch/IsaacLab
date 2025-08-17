b96e8b3bf5982579fa565043dd381fc89a318f1a
Stephen Welch <stephenwelchva@vt.edu>
2025-08-17 06:14:04 -0500

    WIP

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -8,7 +8,7 @@
 # mkdir -p "$CONDA_PREFIX/etc/conda/activate.d"
 # printf 'export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:${LD_LIBRARY_PATH}"\n' > "$CONDA_PREFIX/etc/conda/activate.d/000_set_ld_library_path.sh"
 # conda deactivate && conda activate env_isaaclab
-pip install --upgrade wandb
+# pip install --upgrade wandb
 
 # python scripts/reinforcement_learning/rsl_rl/train.py --headless --log_project_name angmom-loco --logger wandb --task Sandbox-Velocity-Flat-G1-v0 --run_name test
-isaaclab -p scripts/reinforcement_learning/rsl_rl/train.py --headless --log_project_name angmom-loco --logger wandb --task Isaac-Velocity-Flat-G1-v0 --run_name test
+./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --headless --log_project_name angmom-loco --logger wandb --task Isaac-Velocity-Flat-G1-v0 --run_name test
